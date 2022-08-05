4a0c9972d4652d339dadd30edc725be576a50418
Freyder Ortega <fmoz1994@hotmail.com>
2022-08-05 15:50:36 -0500

    se agregan metodos

--- src/logica/Ordenamiento.java
+++ src/logica/Ordenamiento.java
@@ -29,11 +29,12 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         btnOrdenar = new javax.swing.JButton();
+        btnLimpiar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        panel.setBackground(new java.awt.Color(255, 255, 255));
+        panel.setBackground(new java.awt.Color(204, 204, 204));
 
         comboMetodos.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar", "Burbuja", "Insercion", "Seleccion", "Mezcla", "Shellsort", "quicksort" }));
 
@@ -51,6 +52,13 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
             }
         });
 
+        btnLimpiar.setText("Limpiar");
+        btnLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimpiarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
         panel.setLayout(panelLayout);
         panelLayout.setHorizontalGroup(
@@ -59,18 +67,19 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
                 .addContainerGap()
                 .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(panelLayout.createSequentialGroup()
-                        .addComponent(jSeparator1, javax.swing.GroupLayout.DEFAULT_SIZE, 808, Short.MAX_VALUE)
-                        .addContainerGap())
-                    .addGroup(panelLayout.createSequentialGroup()
-                        .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(panelLayout.createSequentialGroup()
-                                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(comboMetodos, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(btnOrdenar))
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 679, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                        .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(comboMetodos, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnOrdenar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnLimpiar)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelLayout.createSequentialGroup()
+                        .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jSeparator1, javax.swing.GroupLayout.DEFAULT_SIZE, 808, Short.MAX_VALUE))
+                        .addContainerGap())))
         );
         panelLayout.setVerticalGroup(
             panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -80,7 +89,8 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
                 .addGap(9, 9, 9)
                 .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(comboMetodos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnOrdenar))
+                    .addComponent(btnOrdenar)
+                    .addComponent(btnLimpiar))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -98,19 +108,23 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
         tipoOrdenamiento = (String) comboMetodos.getSelectedItem();
         switch (tipoOrdenamiento) {
             case "Burbuja":
+                jTextArea1.setText("");
                 burbuja(matriz);
                 break;
             case "Insercion":
+                jTextArea1.setText("");
                 insercion(matriz);
                 break;
             case "Seleccion":
+                jTextArea1.setText("");
                 seleccion(matriz);
                 break;
             case "Mezcla":
 
                 break;
             case "Shellsort":
-
+                jTextArea1.setText("");
+                Shellsort(matriz);
                 break;
             case "quicksort":
 
@@ -122,6 +136,10 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
         }
     }//GEN-LAST:event_btnOrdenarActionPerformed
 
+    private void btnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarActionPerformed
+        jTextArea1.setText("");
+    }//GEN-LAST:event_btnLimpiarActionPerformed
+
     public static void main(String args[]) {
 
         java.awt.EventQueue.invokeLater(() -> {
@@ -130,6 +148,7 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnLimpiar;
     private javax.swing.JButton btnOrdenar;
     public static javax.swing.JComboBox<String> comboMetodos;
     private javax.swing.JLabel jLabel1;
@@ -146,7 +165,6 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
 
             for (int j = 0; j < matriz.length - 1; j++) {
 
-                //  System.out.println(" Se cambia el " + arreglo[j] + " por el " + arreglo[j + 1]);
                 if (matriz[j] > matriz[j + 1]) {
 
                     for (int k = 0; k < matriz.length; k++) {
@@ -155,8 +173,8 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
                         jTextArea1.append(" " + matriz[k]);
 
                     }
-                    //jTextArea1.append(System.getProperty("line.separator"));
-                    jTextArea1.append(" " +" <-- Se cambia el " +matriz[j]+" por el " + matriz[j + 1] +"\n");
+
+                    jTextArea1.append(" " + " <-- Se cambia el " + matriz[j] + " por el " + matriz[j + 1] + "\n");
                     System.out.println(" <-- Se cambia el " + matriz[j] + " por el " + matriz[j + 1]);
 
                     System.out.println("");
@@ -175,6 +193,33 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
 
     @Override
     public void insercion(int[] matriz) {
+        int pos;
+        int aux;
+        try {
+            for (int i = 0; i < matriz.length; i++) {
+                pos = i;
+                aux = matriz[i];
+                for (int k = 0; k < matriz.length; k++) {
+
+                    System.out.print(" " + matriz[k]);
+                    jTextArea1.append(" " + matriz[k]);
+
+                }
+                if (pos > 0) {
+                    jTextArea1.append(" " + " <-- Se cambia el " + matriz[pos] + " por el " + matriz[pos - 1] + "\n");
+                    System.out.println(" <-- Se cambia el " + matriz[pos] + " por el " + matriz[pos - 1]);
+                }
+
+                while ((pos > 0) && (matriz[pos - 1] > aux)) {
+                    matriz[pos] = matriz[pos - 1];
+                    pos--;
+                }
+                matriz[pos] = aux;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error " + e.getMessage());
+        }
+
     }
 
     @Override
@@ -187,6 +232,37 @@ public class Ordenamiento extends javax.swing.JFrame implements Ordena {
 
     @Override
     public void Shellsort(int[] matriz) {
+
+        for (int increment = matriz.length / 2; increment > 0; increment
+                = (increment == 2) ? 1 : (int) Math.round(increment / 2.2)) {
+
+            for (int i = increment; i < matriz.length; i++) {
+                for (int k = 0; k < matriz.length; k++) {
+
+                    System.out.print(" " + matriz[k]);
+                    jTextArea1.append(" " + matriz[k]);
+
+                }
+
+                for (int j = i; j >= increment && matriz[j - increment]
+                        > matriz[j]; j -= increment) {
+                    jTextArea1.append(" " + " <-- Se cambia el " + matriz[j] + " por el " + matriz[j - increment] + "\n");
+                    int temp = matriz[j];
+                    matriz[j] = matriz[j - increment];
+                    matriz[j - increment] = temp;
+
+                }
+
+            }
+        }
+        jTextArea1.append(" Arreglo ordenado \n");
+        for (int k = 0; k < matriz.length; k++) {
+
+            System.out.print(" " + matriz[k]);
+            jTextArea1.append(" " + matriz[k]);
+
+        }
+
     }
 
     @Override
